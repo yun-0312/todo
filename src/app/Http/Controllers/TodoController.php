3f77150ac011add12624d140ca6aabb9334ad7db
yun-0312 <yumiko_03122000@yahoo.co.jp>
2025-10-07 14:43:29 +0900

    todo完成

---
+++ src/app/Http/Controllers/TodoController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+use App\Models\Category;
+use App\Http\Requests\TodoRequest;
+
+class TodoController extends Controller
+{
+    public function index() {
+        $todos = Todo::with('category')->get();
+        $categories = Category::all();
+        return view('index', compact('todos', 'categories'));
+    }
+
+    public function store(TodoRequest $request) {
+        $todo = $request->only(['category_id', 'content']);
+        Todo::create($todo);
+        return redirect('/')->with('message', 'Todoを作成しました');
+    }
+
+    public function update(TodoRequest $request) {
+        $todo = $request->only(['content']);
+        Todo::find($request->id)->update($todo);
+        return redirect('/')->with('message', 'Todoを更新しました');
+    }
+
+    public function destroy(Request $request) {
+        Todo::find($request->id)->delete();
+        return redirect('/')->with('message', 'Todoを削除しました');
+    }
+
+    public function search (Request $request) {
+        $todos = Todo::with('category')->CategorySearch($request->category_id)->keywordSearch($request->keyword)->get();
+        $categories = Category::all();
+        return view('index', compact('todos', 'categories'));
+    }
+}
